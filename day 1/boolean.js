9fc561e33dec237068cf71330553097a6c3d95df
vivek <maltarevivek@gmail.com>
2020-07-29 13:36:06 +0530

    "day1"

--- day 1/boolean.js
+++ day 1/boolean.js
@@ -6,11 +6,13 @@ let var5='';
 let var6=NaN;
 let var7=10;
 let var8=0;
-console.log(Boolean(var1));
-console.log(Boolean(var2));
-console.log(Boolean(var3));
-console.log(Boolean(var4));
-console.log(Boolean(var5));
-console.log(Boolean(var6));
-console.log(Boolean(var7));
-console.log(Boolean(var8));
+let var9=20;
+console.log(Boolean(var1));//true
+console.log(Boolean(var2));//false
+console.log(Boolean(var3));//false
+console.log(Boolean(var4));//false
+console.log(Boolean(var5));//false
+console.log(Boolean(var6));//false
+console.log(Boolean(var7));//true
+console.log(Boolean(var8));//false
+console.log(Boolean(var9));//true
